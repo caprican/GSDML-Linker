35666a3976daa3f231f106180713e288da4ad76b
Morellet <caprican@live.fr>
2024-12-11 21:04:24 +0100

    Fix animation lock parameter

---
+++ GsdmlLinker/Converters/LockVisibiltyConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace GsdmlLinker.Converters;
+
+public class LockVisibiltyConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is Core.Models.DeviceParameter deviceParameter)
+        {
+            return !deviceParameter.IsSelected || deviceParameter?.Subindex > 0 ? Visibility.Collapsed : Visibility.Visible;
+        }
+        return Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
